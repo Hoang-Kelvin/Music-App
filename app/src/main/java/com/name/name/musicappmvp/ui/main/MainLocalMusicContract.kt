cc891aa579244ec689a552f8cc77bfb0cbb934fc
KelvinNguyen <xuongbodep@gmail.com>
2021-01-28 09:19:56 +0700

    update #6

--- app/src/main/java/com/name/name/musicappmvp/ui/main/MainLocalMusicContract.kt
+++ app/src/main/java/com/name/name/musicappmvp/ui/main/MainLocalMusicContract.kt
@@ -1,15 +1,16 @@
 package com.name.name.musicappmvp.ui.main
 
-import com.name.name.musicappmvp.data.model.LocalSong
+import com.name.name.musicappmvp.data.model.Song
 
 class MainLocalMusicContract {
     interface View {
         fun setPlayButton()
         fun setPauseButton()
+        fun displayLocalSong(list: List<Song>)
     }
 
     interface Presenter {
-        fun getAllLocalSong(): List<LocalSong>
+        fun getAllLocalSong()
         fun playChosenSong()
         fun stopSongPlaying()
     }
